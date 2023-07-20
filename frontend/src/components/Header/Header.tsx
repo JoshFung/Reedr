74d5b570c16dc73978bda84110c037ab2d1a6d2f
Josh Fung <joshuagyfung@gmail.com>
2023-07-20 12:35:18 -0700

    Finished quickly styling header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
+import React from "react";
 import "./Header.css";
 
 const Header = () => {
   return (
     <header className="headerContainer">
       <h1 className="headerLogo">Reedr</h1>
-      <ul>
+      <ul className="headerLinkList">
         <li className="headerLink">Home</li>
         <li className="headerLink">Latest</li>
         <li className="headerLink">Ask</li>
