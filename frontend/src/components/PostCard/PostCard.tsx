77227a9b88f2690dacb86e8dcea751971b35412e
Josh Fung <joshuagyfung@gmail.com>
2023-07-24 14:15:52 -0700

    Working on time conversion function

--- frontend/src/components/PostCard/PostCard.tsx
+++ frontend/src/components/PostCard/PostCard.tsx
@@ -1,5 +1,35 @@
 import { Post } from "../../redux/slices/posts/postsSlice";
 
+const timeDifference = (previous: number) => {
+  const diff = Number(new Date()) - previous;
+  // const SEC = 1000; // 1000 milliseconds
+  // const MIN = 60 * SEC; // 60000 milliseconds
+  // const HOUR = 60 * MIN; // 3600000 milliseconds
+  // const DAY = 24 * HOUR; // 86400000 milliseconds
+  // const MONTH = 30 * DAY; // 2592000000 milliseconds
+  // const YEAR = 12 * MONTH; // 31104000000 milliseconds
+  // const timeUnitArray = [YEAR, MONTH, DAY, HOUR, MIN];
+  // timeUnitArray.forEach((timeUnit) => {
+
+  // })
+
+  const timeUnitDict = new Map([
+    [31104000000, "yr"],
+    [2592000000, "mo"],
+    [86400000, "day"],
+    [3600000, "hr"],
+    [60000, "min"],
+  ]);
+
+  timeUnitDict.forEach((timeUnit, timeUnitMilliseconds) => {
+    const timeUnitCount = Math.floor(diff / timeUnitMilliseconds);
+    if (timeUnitCount > 0) {
+      return `${timeUnitCount} ${timeUnit}`;
+    }
+  });
+  return `0 min`;
+};
+
 const PostCard = (props: Post) => {
   return (
     <div className="postcard-container">
