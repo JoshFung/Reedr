f8703fa0f48eca7d8846fcddcbcdebc9d4bcbdff
Josh Fung <joshuagyfung@gmail.com>
2023-07-26 23:35:09 -0700

    Changed route for top 500 to return top 500 IDs

--- backend/dist/controllers/feedController/feedController.js
+++ backend/dist/controllers/feedController/feedController.js
@@ -10,10 +10,11 @@ class FeedController {
             axios_1.default
                 .get("https://hacker-news.firebaseio.com/v0/topstories.json")
                 .then((response) => {
-                console.log("GET Top 500");
+                console.log("GET Top 500 from HN API");
                 res.status(200).send(response.data);
             });
         };
+        // TODO: function to get more than 50 items for infinite scrolling
         this.getById = (req, res) => {
             axios_1.default
                 .get(`https://hacker-news.firebaseio.com/v0/item/${req.params.id}.json`)
