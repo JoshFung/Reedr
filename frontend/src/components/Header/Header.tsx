2e855e259a214b3db6d16f63e3ee496f910fc863
Josh Fung <joshuagyfung@gmail.com>
2023-08-05 18:48:34 -0700

    Converted svg to Icon package

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import "./Header.css";
+import { Icon } from "@iconify/react";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import {
   changeFeedMode,
@@ -17,51 +18,21 @@ const Header = () => {
           className="headerLink"
           onClick={() => dispatch(changeFeedMode(FeedModeEnum.TOP))}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 -3 24 24"
-          >
-            <path
-              fill="currentColor"
-              d="M6 19h3v-6h6v6h3v-9l-6-4.5L6 10v9Zm-2 2V9l8-6l8 6v12h-7v-6h-2v6H4Zm8-8.75Z"
-            />
-          </svg>
+          <Icon icon="material-symbols:home-outline" width="16" height="16" />
           Home
         </li>
         <li
           className="headerLink"
           onClick={() => dispatch(changeFeedMode(FeedModeEnum.NEW))}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 -3 24 24"
-          >
-            <path
-              fill="currentColor"
-              d="M13.5 8H12v5l4.28 2.54l.72-1.21l-3.5-2.08V8M13 3a9 9 0 0 0-9 9H1l3.96 4.03L9 12H6a7 7 0 0 1 7-7a7 7 0 0 1 7 7a7 7 0 0 1-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42A8.896 8.896 0 0 0 13 21a9 9 0 0 0 9-9a9 9 0 0 0-9-9"
-            />
-          </svg>
+          <Icon icon="mdi:history" width="16" height="16" />
           Latest
         </li>
         <li
           className="headerLink"
           onClick={() => dispatch(changeFeedMode(FeedModeEnum.ASK))}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 -4 24 24"
-          >
-            <path
-              fill="currentColor"
-              d="M4 4h16v12H5.17L4 17.17V4m0-2c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2H4zm2 10h8v2H6v-2zm0-3h12v2H6V9zm0-3h12v2H6V6z"
-            />
-          </svg>
+          <Icon icon="ic:outline-chat" width="16" height="16" />
           Ask
         </li>
       </ul>
--- frontend/src/components/PostCard/PostCard.tsx
+++ frontend/src/components/PostCard/PostCard.tsx
@@ -1,4 +1,5 @@
 import { Post } from "../../redux/slices/posts/postsSlice";
+import { Icon } from "@iconify/react";
 import "./PostCard.css";
 
 const PostCard = (props: Post) => {
@@ -34,63 +35,39 @@ const PostCard = (props: Post) => {
       <div className="postcard-title">{title}</div>
       <div className="postcard-info-container">
         <div className="postcard-info-piece postcard-points-container">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
+          <Icon
+            icon="mdi:arrow-up-bold-outline"
             width="12"
             height="12"
-            viewBox="0 0 24 24"
             className="postcard-info-icon"
-          >
-            <path
-              fill="currentColor"
-              d="M16 13v8H8v-8H2L12 3l10 10h-6m-9-2h3v8h4v-8h3l-5-5l-5 5Z"
-            />
-          </svg>
+          />
           {score.toString()}
         </div>
         <div className="postcard-info-piece postcard-comments-container">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
+          <Icon
+            icon="mdi:comment-outline"
             width="12"
             height="12"
-            viewBox="0 0 24 24"
             className="postcard-info-icon"
-          >
-            <path
-              fill="currentColor"
-              d="M9 22a1 1 0 0 1-1-1v-3H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2h-6.1l-3.7 3.71c-.2.19-.45.29-.7.29H9m1-6v3.08L13.08 16H20V4H4v12h6Z"
-            />
-          </svg>
+          />
           {kids ? kids.length : 0}
         </div>
         <div className="postcard-info-piece postcard-time-container">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
+          <Icon
+            icon="mdi:clock-outline"
             width="12"
             height="12"
-            viewBox="0 0 24 24"
             className="postcard-info-icon"
-          >
-            <path
-              fill="currentColor"
-              d="M12 20a8 8 0 0 0 8-8a8 8 0 0 0-8-8a8 8 0 0 0-8 8a8 8 0 0 0 8 8m0-18a10 10 0 0 1 10 10a10 10 0 0 1-10 10C6.47 22 2 17.5 2 12A10 10 0 0 1 12 2m.5 5v5.25l4.5 2.67l-.75 1.23L11 13V7h1.5Z"
-            />
-          </svg>
+          />
           {convertedTime}
         </div>
         <div className="postcard-info-piece postcard-author-container">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
+          <Icon
+            icon="mdi:account-outline"
             width="12"
             height="12"
-            viewBox="0 0 24 24"
             className="postcard-info-icon"
-          >
-            <path
-              fill="currentColor"
-              d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 2a2 2 0 0 0-2 2a2 2 0 0 0 2 2a2 2 0 0 0 2-2a2 2 0 0 0-2-2m0 7c2.67 0 8 1.33 8 4v3H4v-3c0-2.67 5.33-4 8-4m0 1.9c-2.97 0-6.1 1.46-6.1 2.1v1.1h12.2V17c0-.64-3.13-2.1-6.1-2.1Z"
-            />
-          </svg>
+          />
           {by}
         </div>
       </div>
