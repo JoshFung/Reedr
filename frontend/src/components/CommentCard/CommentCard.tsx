53647b411f36ffdb18a5fb74e95ff02e5d4e16fb
Josh Fung <joshuagyfung@gmail.com>
2023-08-11 16:12:23 -0700

    Skeleton CommentCard with css

---
+++ frontend/src/components/CommentCard/CommentCard.tsx
@@ -0,0 +1,16 @@
+import "./CommentCard.css";
+
+const CommentCard = () => {
+  <div className="comment">
+    <div className="comment-bar"></div>
+    <div className="comment-content">
+      <p className="comment-text"></p>
+      <div className="comment-info">
+        <div className="comment-author-date"></div>
+        <div className="comment-children"></div>
+      </div>
+    </div>
+  </div>;
+};
+
+export default CommentCard;
