dbdd556db6f93ba0d4e3a0cd3504b427c01dcf27
Josh Fung <joshuagyfung@gmail.com>
2023-08-15 02:07:53 -0700

    Improved text parsing for comments

--- frontend/src/components/CommentCard/CommentCard.tsx
+++ frontend/src/components/CommentCard/CommentCard.tsx
@@ -2,18 +2,28 @@ import { Icon } from "@iconify/react";
 import { Comment } from "../../redux/slices/post/postSlice";
 import { timeDifference } from "../../utils/helpers";
 import "./CommentCard.css";
+import he from "he";
+import parse from "html-react-parser";
 
 const CommentCard = (props: Comment) => {
   const { by, time, text, kids } = props;
   const convertedTime = timeDifference(time, true);
   const directChildren = kids?.length ?? 0;
+  console.log(props);
+
+  const convertText = (text: string) => {
+    console.log(text);
+    const convertChars = he.decode(text);
+    const convertTags = convertChars.replace(/<p>/g, "\n\n");
+    return parse(convertTags);
+  };
 
   return (
     <div className="comment-container">
       <div className="comment">
         {/* <div className="comment-bars"></div> */}
         <div className="comment-content">
-          <p className="comment-text">{text}</p>
+          <p className="comment-text">{convertText(text)}</p>
           <div className="comment-info">
             <div className="comment-author-date">
               By
