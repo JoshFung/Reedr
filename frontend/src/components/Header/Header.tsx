1eafc88111983b72885c2d040785c0525bc8e88a
Josh Fung <joshuagyfung@gmail.com>
2023-08-18 19:33:56 -0700

    Updated header to have all feed modes

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,18 +1,18 @@
 import "./Header.css";
 import { Icon } from "@iconify/react";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-import { setFeedMode, FeedModeEnum } from "../../redux/slices/feed/feedSlice";
+import { setFeedMode } from "../../redux/slices/feed/feedSlice";
+import { FeedModeEnum } from "../../redux/slices/feed/FeedModeEnum";
 import {
   selectSelectedPost,
   setNoPost,
 } from "../../redux/slices/post/postSlice";
-import { useEffect } from "react";
 
 const Header = () => {
   const dispatch = useAppDispatch();
   const selectedPost = useAppSelector(selectSelectedPost);
 
-  const isClickable = selectedPost !== null;
+  const readerOpen = selectedPost !== null;
 
   const handleTitleClick = () => {
     dispatch(setNoPost());
@@ -22,31 +22,56 @@ const Header = () => {
     <header className="header-container">
       <h2
         className="header-title"
-        onClick={isClickable ? handleTitleClick : undefined}
+        onClick={readerOpen ? handleTitleClick : undefined}
       >
         {selectedPost ? "Close" : "Reedr"}
       </h2>
-      <ul className="header-link-list">
+      <ul className="header-feed-list">
         <li
           className="header-link"
           onClick={() => dispatch(setFeedMode(FeedModeEnum.TOP))}
         >
-          <Icon icon="material-symbols:home-outline" width="16" height="16" />
+          <Icon
+            icon="material-symbols:home-outline"
+            width="1rem"
+            height="1rem"
+          />
           Home
         </li>
         <li
           className="header-link"
           onClick={() => dispatch(setFeedMode(FeedModeEnum.NEW))}
         >
-          <Icon icon="mdi:history" width="16" height="16" />
-          Latest
+          <Icon icon="mdi:history" width="1rem" height="1rem" />
+          New
+        </li>
+        <li
+          className="header-link"
+          onClick={() => dispatch(setFeedMode(FeedModeEnum.BEST))}
+        >
+          <Icon icon="mdi:star-outline" width="1rem" height="1rem" />
+          Best
         </li>
         <li
           className="header-link"
           onClick={() => dispatch(setFeedMode(FeedModeEnum.ASK))}
         >
-          <Icon icon="ic:outline-chat" width="16" height="16" />
+          <Icon icon="mdi:help" width="1rem" height="1rem" />
           Ask
+        </li>
+        <li
+          className="header-link"
+          onClick={() => dispatch(setFeedMode(FeedModeEnum.SHOW))}
+        >
+          <Icon icon="mdi:bullhorn-outline" width="1rem" height="1rem" />
+          Show
+        </li>
+        <li
+          className="header-link"
+          onClick={() => dispatch(setFeedMode(FeedModeEnum.JOB))}
+        >
+          <Icon icon="mdi:briefcase-outline" width="1rem" height="1rem" />
+          Jobs
         </li>
       </ul>
     </header>
