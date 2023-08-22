ae93ff9aea22d1a40333a20e4a66d9b02337a1de
Josh Fung <joshuagyfung@gmail.com>
2023-08-22 00:37:15 -0700

    Improved hover and onClick highlighting for postCards

--- frontend/src/components/PostCard/PostCard.tsx
+++ frontend/src/components/PostCard/PostCard.tsx
@@ -1,14 +1,27 @@
-import { Post, setSelectedPost } from "../../redux/slices/post/postSlice";
-import { useAppDispatch } from "../../redux/hooks";
+import {
+  Post,
+  selectSelectedPost,
+  setSelectedPost,
+} from "../../redux/slices/post/postSlice";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { timeDifference } from "../../utils/helpers";
 import { Icon } from "@iconify/react";
 import "./PostCard.css";
 
 const PostCard = (props: Post) => {
   const dispatch = useAppDispatch();
+  const selectedPost = useAppSelector(selectSelectedPost);
 
-  const { title, score, time, by, descendants } = props;
+  const { id, title, score, time, by, descendants } = props;
   const convertedTime = timeDifference(time, false);
+  let selectedStyle: React.CSSProperties = {};
+
+  if (selectedPost && selectedPost.id === id) {
+    selectedStyle = {
+      border: "2px solid #ff9500",
+      boxShadow: "-0.25rem 0.25rem 0.25rem 0rem #ff9500",
+    };
+  }
 
   return (
     <div
@@ -16,6 +29,7 @@ const PostCard = (props: Post) => {
       onClick={() => {
         dispatch(setSelectedPost(props));
       }}
+      style={selectedStyle}
     >
       <div className="postcard-title">{title}</div>
       <div className="postcard-info-container">
